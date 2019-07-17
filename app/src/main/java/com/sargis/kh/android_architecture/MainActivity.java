705470ee1eb6ab74ad6e671a93a9f9593a5991a9
SargisKhlopuzyan <sargis.khlopuzyan@gmail.com>
2019-07-17 17:05:17 +0400

    Code cleaning

--- app/src/main/java/com/sargis/kh/android_architecture/MainActivity.java
+++ app/src/main/java/com/sargis/kh/android_architecture/MainActivity.java
@@ -1,9 +1,8 @@
 package com.sargis.kh.android_architecture;
 
 import android.databinding.DataBindingUtil;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 
 import com.sargis.kh.android_architecture.databinding.ActivityMainBinding;
 
